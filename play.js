aa70deab78c2f3771d7082ba341c050904ac13d2
Md Ashikur Rahman <arshakib51@gmail.com>
2023-05-19 00:27:28 +0600

    Add function for using it by clicking on mobile and pc

--- play.js
+++ play.js
@@ -16,3 +16,15 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
+
+keys.forEach(key => key.addEventListener('click', playSoundOnclick));
+// function for using it by clicking on mobile and pc
+function playSoundOnclick(e){
+    let keyCode = this.getAttribute('data-key');
+    const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${keyCode}']`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
